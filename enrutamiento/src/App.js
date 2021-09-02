2c34bda62a58f4e71114dd1a7d178405ca733bf0
danielmejias <82547292+danielmejias@users.noreply.github.com>
2021-09-02 17:20:27 -0400

    Initial Load

---
+++ enrutamiento/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          {/* <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul> */}
+        </nav>
+        <Switch>
+          <Route path="/4">the number is:4</Route>
+          {/* <Route path="/users">
+            <Users />
+          </Route> */}
+          <Route path="/">
+            {/* <Home /> */}
+            Bienvenida
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
